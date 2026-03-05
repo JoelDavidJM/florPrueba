a5e5ac7f2d8d7e1cb15146b8c2a668241cb366d3
Joel Játiva <150308784+JoelDavidJM@users.noreply.github.com>
2026-03-04 20:28:00 -0500

    grass

--- ejercicio.js
+++ ejercicio.js
@@ -1,21 +1,32 @@
-let number = 9
-let number2 = 0
-let operacion = '/'
-
-if (operacion === '+') {
-    let result = number + number2
-    console.log(result)
-} else if (operacion === '-') {
-    let result = number - number2
-    console.log(result)
-} else if (operacion === '*') {
-    let result = number * number2
-    console.log(result)
-} else if (operacion === '/') {
-    if (number2 === 0) {
-        console.log('No se puede dividir entre cero')
-    } else {
-        let result = number / number2
-        console.log(result)
+const clases = [
+    {
+        numero: 1,
+        titulo: 'class 1',
+        descripcion: 'variable'
+    },
+    {
+        numero: 2,
+        titulo: 'class 2',
+        descripcion: 'variable'
+    },
+    {
+        numero: 3,
+        titulo: 'class 3',
+        descripcion: 'variable'
+    },
+    {
+        numero: 4,
+        titulo: 'class 4',
+        descripcion: 'variable'
+    },
+    {
+        numero: 5,
+        titulo: 'class 5',
+        descripcion: 'variable'
     }
-}
+]
+
+// fghjklyhjk
+const titulos = clases.map((clase) => clase.titulo.toUpperCase());
+
+console.log(titulos)
